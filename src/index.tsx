c377c661e97509da95cf9dedb7a02ecb3f94e5e4
Andrew Fong <afong@slack-corp.com>
2024-12-16 12:46:29 -0800

    Grid

--- src/index.tsx
+++ src/index.tsx
@@ -10,44 +10,77 @@ import {
 	Card,
 	CardContent,
 	CardDescription,
+	CardFooter,
 	CardHeader,
 	CardTitle,
 } from '~/shared/components/card';
+import { Grid } from '~/shared/components/grid';
 import { Group } from '~/shared/components/group';
 import { Stack } from '~/shared/components/stack';
 
 const App: Component = () => {
 	return (
 		<Box>
-			<Card>
-				<CardHeader>
-					<CardTitle>Buttons</CardTitle>
-					<CardDescription>Different variations on your standard button</CardDescription>
-				</CardHeader>
-				<CardContent>
-					<Stack>
-						<Group>
-							<Button class="c-btn--sm">
-								<Settings /> Small Button
-							</Button>
+			<Grid>
+				<Card class="col-span-full">
+					<CardHeader>
+						<CardTitle>Buttons</CardTitle>
+						<CardDescription>
+							Different variations on your standard button
+						</CardDescription>
+					</CardHeader>
+					<CardContent>
+						<Stack>
+							<Group class="flex-wrap">
+								<Button class="c-btn--sm">
+									<Settings /> Small Button
+								</Button>
 
-							<Button>
-								<Settings /> Default Button
-							</Button>
+								<Button>
+									<Settings /> Default Button
+								</Button>
 
-							<Button class="c-btn--lg">
-								<Settings /> Large Button
-							</Button>
-						</Group>
-						<Group>
-							<Button class="c-btn--primary">Primary</Button>
-							<Button class="c-btn--danger">Danger</Button>
-							<Button class="c-btn--ghost">Ghost</Button>
-							<Button class="c-btn--link">Link</Button>
-						</Group>
-					</Stack>
-				</CardContent>
-			</Card>
+								<Button class="c-btn--lg">
+									<Settings /> Large Button
+								</Button>
+							</Group>
+							<Group>
+								<Button class="c-btn--primary">Primary</Button>
+								<Button class="c-btn--danger">Danger</Button>
+								<Button class="c-btn--ghost">Ghost</Button>
+								<Button class="c-btn--link">Link</Button>
+							</Group>
+						</Stack>
+					</CardContent>
+				</Card>
+
+				<Card>
+					<CardHeader>
+						<CardTitle>Card Two</CardTitle>
+						<CardDescription>Another example card in the grid</CardDescription>
+					</CardHeader>
+					<CardContent>
+						<p>This card demonstrates the responsive grid layout.</p>
+					</CardContent>
+					<CardFooter>
+						<Button class="c-btn--primary">Learn More</Button>
+					</CardFooter>
+				</Card>
+
+				<Card>
+					<CardHeader>
+						<CardTitle>Card Three</CardTitle>
+						<CardDescription>A third card to show the grid in action</CardDescription>
+					</CardHeader>
+					<CardContent>
+						<p>The grid will adjust columns based on screen size.</p>
+					</CardContent>
+					<CardFooter>
+						<Button class="c-btn--ghost">Skip</Button>
+						<Button class="c-btn--primary">Continue</Button>
+					</CardFooter>
+				</Card>
+			</Grid>
 		</Box>
 	);
 };
---
+++ src/shared/components/grid.tsx
@@ -0,0 +1,6 @@
+import cx from 'classix';
+import { type JSX } from 'solid-js';
+
+export function Grid(props: JSX.IntrinsicAttributes & JSX.HTMLAttributes<HTMLDivElement>) {
+	return <div {...props} class={cx('c-grid', props.class)} />;
+}
