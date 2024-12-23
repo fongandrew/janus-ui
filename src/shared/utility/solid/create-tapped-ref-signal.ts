2264af2088ea6ebde5f5dd936c90c5365aac2fea
Andrew Fong <afong@slack-corp.com>
2024-12-23 10:09:27 -0800

    Tweak tapped ref typing

--- src/shared/utility/solid/create-tapped-ref-signal.ts
+++ src/shared/utility/solid/create-tapped-ref-signal.ts
@@ -1,15 +1,15 @@
-import { createSignal } from 'solid-js';
+import { createSignal, type Setter } from 'solid-js';
 
 /**
  * A Solid JS utility that, when given a callback function for a ref, returns a signal
  * and setter (that can be used as a ref itself)
  */
-export function createTappedRefSignal<TRef>(callback: (value: TRef) => void) {
+export function createTappedRefSignal<TRef>(callback: Setter<TRef> | ((val: TRef) => void)) {
 	const [signal, setSignal] = createSignal<TRef>();
 	return [
 		signal,
 		(value: TRef) => {
-			callback(value);
+			(callback as (val: TRef) => void)(value);
 			// Appease TypeScript over setter accepting both functional and static forms
 			setSignal(() => value);
 		},
