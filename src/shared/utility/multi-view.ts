070a27cd0db8e66bca0f621624fc00b0c20b00a9
Andrew Fong <afong@slack-corp.com>
2025-01-13 09:02:13 -0800

    Multi window helpers

---
+++ src/shared/utility/multi-view.ts
@@ -0,0 +1,34 @@
+/**
+ * Util for getting document from a node. Useful to support multiple documents
+ */
+export function elmDoc(node: HTMLElement): Document;
+export function elmDoc(node: HTMLElement | null | undefined): Document | undefined;
+export function elmDoc(node: HTMLElement | null | undefined): Document | undefined {
+	return node?.ownerDocument;
+}
+
+/**
+ * Util for getting window from a node. Useful to support multiple windows. Window
+ * is usually present but technically may be undefined.
+ */
+export function elmWin(node: HTMLElement): Window | null;
+export function elmWin(node: HTMLElement | null | undefined): Window | null | undefined;
+export function elmWin(node: HTMLElement | null | undefined): Window | null | undefined {
+	return elmDoc(node)?.defaultView;
+}
+
+/**
+ * Get document from event target.
+ */
+export function evtDoc(evt: Event): Document | undefined {
+	return elmDoc(evt?.target as HTMLElement | undefined | null);
+}
+
+/**
+ * Get window from event target. Technically, events may not have a target
+ * or the target may not have a ownerDocument, but this doesn't happen if
+ * used in an event handler so we're going to just force return type as Window.
+ */
+export function evtWin(evt: Event): Window | null | undefined {
+	return elmWin(evt?.target as HTMLElement | undefined | null);
+}
