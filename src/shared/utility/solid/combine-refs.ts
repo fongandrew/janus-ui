f4540fe36fed5cb52ef12cbe615edf6ca55a93f2
Andrew Fong <afong@slack-corp.com>
2025-01-15 15:13:46 -0800

    Make combineRefs more flexible

--- src/shared/utility/solid/combine-refs.ts
+++ src/shared/utility/solid/combine-refs.ts
@@ -2,11 +2,16 @@
  * Combines multiple Solid ref callbacks
  */
 export function combineRefs<T>(
-	...refs: (((val: T) => void) | null | undefined)[]
+	...refs: (((val: T) => void) | T | null | undefined)[]
 ): (val: T) => void {
 	return (val: T) => {
 		for (const ref of refs) {
-			ref?.(val);
+			// Solid allows `let` variable assignment in JSX, which results in the ref
+			// being typed as `T` itself, but Solid will actually pass a function as
+			// the actual props.ref the first time it's called, so we can work with that.
+			if (typeof ref === 'function') {
+				(ref as (val: T) => void)(val);
+			}
 		}
 	};
 }
