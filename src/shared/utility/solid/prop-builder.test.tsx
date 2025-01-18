f1c5d971af145bbefc734eb395e84ae4cee49629
Andrew Fong <afong@slack-corp.com>
2025-01-18 14:31:12 -0800

    Prop Builder for making controls

---
+++ src/shared/utility/solid/prop-builder.test.tsx
@@ -0,0 +1,191 @@
+import { fireEvent, render, screen } from '@solidjs/testing-library';
+import { type Component, createSignal } from 'solid-js';
+
+import { PropBuilder } from '~/shared/utility/solid/prop-builder';
+
+describe('PropBuilder', () => {
+	it('should merge refs', async () => {
+		const builder = new PropBuilder<HTMLDivElement>();
+		let refValue: HTMLDivElement | undefined;
+
+		const Parent: Component = () => {
+			builder.addRef((el) => {
+				refValue = el;
+			});
+			return <Child />;
+		};
+
+		const Child: Component = () => {
+			return <div {...builder.merge({ 'data-testid': 'test-div' })}>Test</div>;
+		};
+
+		render(() => <Parent />);
+		const div = screen.getByTestId('test-div');
+
+		expect(refValue).toBe(div);
+		expect(builder.ref()).toBe(div);
+	});
+
+	it('should extend attributes and track reactivity', () => {
+		const builder = new PropBuilder<HTMLDivElement>();
+
+		const [dynamicClass, setDynamicClass] = createSignal('test-class');
+		const [baseClass, setBaseClass] = createSignal('base-class');
+
+		const Parent: Component = () => {
+			builder.extAttr('class', () => dynamicClass());
+			builder.extAttr('class', () => 'another-class');
+			return <Child class={baseClass()} />;
+		};
+
+		const Child: Component<{ class?: string }> = (props) => {
+			return (
+				<div {...builder.merge({ class: props.class, 'data-testid': 'test-div' })}>
+					Test
+				</div>
+			);
+		};
+
+		render(() => <Parent />);
+		const div = screen.getByTestId('test-div');
+
+		expect(div.className).toBe('base-class test-class another-class');
+
+		setDynamicClass('updated-class');
+		expect(div.className).toBe('base-class updated-class another-class');
+
+		setBaseClass('new-base-class');
+		expect(div.className).toBe('new-base-class updated-class another-class');
+	});
+
+	it('should set attributes and track reactivity', () => {
+		const builder = new PropBuilder<HTMLDivElement>();
+
+		const [label, setLabel] = createSignal('test label');
+		const [childLabel, setChildLabel] = createSignal('child label');
+
+		const Parent: Component = () => {
+			builder.setAttr('aria-label', () => label());
+			return <Child ariaLabel={childLabel()} />;
+		};
+
+		const Child: Component<{ ariaLabel?: string }> = (props) => {
+			return (
+				<div
+					{...builder.merge({
+						'aria-label': props.ariaLabel,
+						'data-testid': 'test-div',
+					})}
+				>
+					Test
+				</div>
+			);
+		};
+
+		render(() => <Parent />);
+		const div = screen.getByTestId('test-div');
+
+		// Parent's setAttr takes precedence over Child's prop
+		expect(div.getAttribute('aria-label')).toBe('test label');
+
+		setLabel('updated label');
+		expect(div.getAttribute('aria-label')).toBe('updated label');
+
+		// Child's prop change should not affect the attribute since Parent's setAttr takes precedence
+		setChildLabel('new child label');
+		expect(div.getAttribute('aria-label')).toBe('updated label');
+	});
+
+	it('should handle events', () => {
+		const builder = new PropBuilder<HTMLDivElement>();
+		const clicks: number[] = [];
+
+		const Parent: Component = () => {
+			builder.handle('onClick', () => clicks.push(1));
+			builder.handle('onClick', () => clicks.push(2));
+			return <Child />;
+		};
+
+		const Child: Component = () => {
+			return (
+				<div
+					{...builder.merge({
+						'data-testid': 'test-div',
+						onClick: () => clicks.push(3),
+					})}
+				>
+					Test
+				</div>
+			);
+		};
+
+		render(() => <Parent />);
+		const div = screen.getByTestId('test-div');
+
+		fireEvent.click(div);
+		expect(clicks).toEqual([1, 2, 3]);
+	});
+
+	it('should track id changes', () => {
+		const builder = new PropBuilder<HTMLDivElement>();
+		const [id, setId] = createSignal('initial');
+
+		const Parent: Component = () => {
+			return <Child />;
+		};
+
+		const Child: Component = () => {
+			return <div {...builder.merge({ id: id(), 'data-testid': 'test-div' })}>Test</div>;
+		};
+
+		render(() => <Parent />);
+		const div = screen.getByTestId('test-div');
+
+		expect(div.id).toBe('initial');
+		expect(builder.id()).toBe('initial');
+
+		setId('updated');
+		expect(div.id).toBe('updated');
+		expect(builder.id()).toBe('updated');
+	});
+
+	it('should merge all props together', () => {
+		const builder = new PropBuilder<HTMLDivElement>();
+		const clicked = { value: false };
+
+		const Parent: Component = () => {
+			builder.extAttr('class', () => 'ext-class');
+			builder.setAttr('aria-label', () => 'test label');
+			builder.handle('onClick', () => {
+				clicked.value = true;
+			});
+			return <Child />;
+		};
+
+		const Child: Component = () => {
+			return (
+				<div
+					{...builder.merge({
+						id: 'test-id',
+						class: 'base-class',
+						style: 'color: red',
+						'data-testid': 'test-div',
+					})}
+				>
+					Test
+				</div>
+			);
+		};
+
+		render(() => <Parent />);
+		const div = screen.getByTestId('test-div');
+
+		expect(div.id).toBe('test-id');
+		expect(div.className).toBe('base-class ext-class');
+		expect(div.style.color).toBe('red');
+		expect(div.getAttribute('aria-label')).toBe('test label');
+
+		fireEvent.click(div);
+		expect(clicked.value).toBe(true);
+	});
+});
---
+++ src/shared/utility/solid/prop-builder.ts
@@ -0,0 +1,153 @@
+import { createRenderEffect, createSignal, type JSX, mergeProps, onCleanup } from 'solid-js';
+
+import { pullLast } from '~/shared/utility/list';
+import { bindProp, combineEventHandlers } from '~/shared/utility/solid/combine-event-handlers';
+import { combineRefs } from '~/shared/utility/solid/combine-refs';
+import { createIncrSignal } from '~/shared/utility/solid/create-incr-signal';
+
+/**
+ * A class used to additively declare props that should be merged onto a given
+ * component or element. Expected usage is for a much higher parent or sibling
+ * element to try and assign props, or if we want to refactor some prop logic
+ * across multiple components.
+ */
+export class PropBuilder<T extends HTMLElement> {
+	// Disable linter below because it's complaining it wants the destructured values
+	// from `createSignal` but we know what we're doing here.
+	// eslint-disable-next-line solid/reactivity
+	private readonly idSig = createSignal<string>();
+	// eslint-disable-next-line solid/reactivity
+	private readonly refSig = createSignal<T>();
+
+	/** Extra ref assignment callbacks */
+	private readonly refCbs: ((val: T) => void)[] = [];
+	/** Callbacks that return a non-exclusive list item to a prop */
+	private readonly attrListCbs: Record<string, (() => string | undefined)[]> = {};
+	/** Callbacks that exclusively set the value of a prop */
+	private readonly attrCbs: Record<string, (() => string | boolean | number | undefined)[]> = {};
+	/** Event handler callbacks */
+	private readonly evtCbs: Record<string, JSX.EventHandlerUnion<T, Event>[]> = {};
+
+	/** Signal accessor that updates when the prop ID changes */
+	id = this.idSig[0];
+
+	/** Signal accessor that returns a ref to the underlying element */
+	ref = this.refSig[0];
+
+	/** Add a ref function that will be added to the underlying component */
+	addRef(callback: ((val: T) => void) | T | null | undefined) {
+		if (typeof callback === 'function') {
+			this.refCbs.push(callback);
+		}
+		return this;
+	}
+
+	/** Add a string ID to an attribute that is a list of IDs */
+	extAttr<TAttr extends keyof JSX.HTMLAttributes<T>>(
+		attr: TAttr & (JSX.HTMLAttributes<T>[TAttr] extends string | undefined ? TAttr : never),
+		callback: () => JSX.HTMLAttributes<T>[TAttr],
+	) {
+		const list = (this.attrListCbs[attr] ??= []);
+		list.push(callback);
+		onCleanup(() => pullLast(list, callback));
+	}
+
+	/** Assign a string attribute to an element */
+	setAttr<TAttr extends keyof JSX.HTMLAttributes<T>>(
+		attr: TAttr &
+			(JSX.HTMLAttributes<T>[TAttr] extends boolean | number | string | undefined
+				? TAttr
+				: never),
+		callback: () => JSX.HTMLAttributes<T>[TAttr],
+	) {
+		const list = (this.attrCbs[attr] ??= []);
+		list.push(callback);
+		onCleanup(() => pullLast(list, callback));
+	}
+
+	/** Add to a callback prop like onClick */
+	handle<TProp extends keyof JSX.CustomEventHandlersCamelCase<T>>(
+		prop: TProp,
+		handler: JSX.CustomEventHandlersCamelCase<T>[TProp],
+	) {
+		if (!handler) return;
+		const list = (this.evtCbs[prop] ??= []);
+		list.push(handler as JSX.EventHandlerUnion<T, Event>);
+		onCleanup(() => pullLast(list, handler as JSX.EventHandlerUnion<T, Event>));
+	}
+
+	/**
+	 * Merge props based on all the extensions that have been added.
+	 */
+	merge<TProps extends JSX.HTMLAttributes<T>>(
+		props: TProps & Record<`data-${string}`, string>,
+	): TProps {
+		const [track, signalUpdate] = createIncrSignal();
+		const ret = {} as Record<string, unknown> & Record<`data-${string}`, string>;
+		const update = (key: string, value: unknown) => {
+			if (ret[key] !== value) {
+				ret[key] = value;
+				signalUpdate();
+			}
+		};
+
+		// Set up ref to capture element
+		const [, setRef] = this.refSig;
+		ret['ref'] = combineRefs(
+			setRef,
+			...(this.refCbs as (((val: T) => void) | T | null | undefined)[]),
+			// Uh, just don't change the ref prop. We can do something hacky if needed
+			// but honestly it's a bit of hassle and overhead and all of the overhead
+			// for this could live in the ref itself if need be.
+			// eslint-disable-next-line solid/reactivity
+			props.ref,
+		);
+
+		// Update ID signal when ID changes
+		createRenderEffect(() => {
+			const [, setId] = this.idSig;
+			setId(props.id);
+		});
+
+		// Merge attribute lists
+		for (const [attr, callbacks] of Object.entries(this.attrListCbs)) {
+			createRenderEffect(() => {
+				const propValue = props[attr as keyof typeof props] as string | undefined;
+				const values: string[] = propValue ? [propValue] : [];
+				for (const callback of callbacks) {
+					const ret = callback();
+					if (ret) {
+						values.push(ret);
+					}
+				}
+				update(attr, values.join(' '));
+			});
+		}
+
+		// Merge attributes
+		for (const [attr, callbacks] of Object.entries(this.attrCbs)) {
+			createRenderEffect(() => {
+				const last = callbacks[callbacks.length - 1];
+				update(attr, last?.());
+			});
+		}
+
+		// Merge event handlers
+		for (const [prop, handlers] of Object.entries(this.evtCbs)) {
+			ret[prop] = combineEventHandlers(
+				...handlers,
+				bindProp(props, prop as keyof JSX.HTMLAttributes<T>) as JSX.EventHandlerUnion<
+					T,
+					Event
+				>,
+			);
+		}
+
+		const merged = mergeProps(props, () => {
+			track();
+			// Spread object to ensure we're returning a new object
+			return { ...(ret as TProps) };
+		});
+		return merged as TProps;
+	}
+}
