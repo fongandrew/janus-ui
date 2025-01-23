59c856439528f336fb63b52b95caccba4d5933c4
Andrew Fong <afong@slack-corp.com>
2025-01-23 15:24:32 -0800

    Circular

--- src/shared/components/spinner.tsx
+++ src/shared/components/spinner.tsx
@@ -1,10 +1,10 @@
 import cx from 'classix';
-import { Loader, type LucideProps } from 'lucide-solid';
+import { Loader2, type LucideProps } from 'lucide-solid';
 import { type Component, splitProps } from 'solid-js';
 
 export interface SpinnerProps extends LucideProps {}
 
 export const Spinner: Component<SpinnerProps> = (props) => {
 	const [local, rest] = splitProps(props, ['class']);
-	return <Loader {...rest} class={cx('animate-spin', local.class)} />;
+	return <Loader2 {...rest} class={cx('animate-spin', local.class)} />;
 };
