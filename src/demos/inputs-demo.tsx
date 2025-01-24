f8e168c9e121d2807da69964a930ee41df9027ad
Andrew Fong <afong@slack-corp.com>
2025-01-23 22:07:49 -0800

    Demo different input types

--- src/demos/inputs-demo.tsx
+++ src/demos/inputs-demo.tsx
@@ -1,5 +1,3 @@
-import { type Component } from 'solid-js';
-
 import {
 	Card,
 	CardContent,
@@ -7,40 +5,146 @@ import {
 	CardHeader,
 	CardTitle,
 } from '~/shared/components/card';
-import { Input, InputDate, InputTime } from '~/shared/components/input';
+import {
+	Input,
+	InputDate,
+	InputDateTime,
+	InputMonth,
+	InputTime,
+	InputWeek,
+} from '~/shared/components/input';
 import { Label } from '~/shared/components/label';
 import { LabelStack } from '~/shared/components/label-stack';
+import { LabelledControl } from '~/shared/components/labelled-control';
 import { Stack } from '~/shared/components/stack';
 
-export const InputsDemo: Component = () => (
-	<Card>
-		<CardHeader>
-			<CardTitle>Inputs</CardTitle>
-			<CardDescription>Text input fields in different states</CardDescription>
-		</CardHeader>
-		<CardContent>
-			<Stack>
-				<LabelStack>
-					<Label>Default Input</Label>
-					<Input placeholder="Placeholder content" />
-				</LabelStack>
-				<LabelStack>
-					<Label>Error State Input</Label>
-					<Input aria-invalid="true" placeholder="Some wrong value" />
-				</LabelStack>
-				<LabelStack>
-					<Label>Disabled Input</Label>
-					<Input disabled placeholder="Can't touch this" />
-				</LabelStack>
-				<LabelStack>
-					<Label>Date Input</Label>
-					<InputDate placeholder="Pick a date" value="2024-12-25" />
-				</LabelStack>
-				<LabelStack>
-					<Label>Time Input</Label>
-					<InputTime placeholder="Pick a time" value="13:15" />
-				</LabelStack>
-			</Stack>
-		</CardContent>
-	</Card>
-);
+function InputStates() {
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle>Input States</CardTitle>
+				<CardDescription>Text input fields in different states</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<Stack>
+					<LabelStack>
+						<Label>Default Input</Label>
+						<Input placeholder="Placeholder content" />
+					</LabelStack>
+					<LabelStack>
+						<Label>Error State Input</Label>
+						<Input aria-invalid="true" placeholder="Some wrong value" />
+					</LabelStack>
+					<LabelStack>
+						<Label>Disabled Input</Label>
+						<Input disabled placeholder="Can't touch this" />
+					</LabelStack>
+				</Stack>
+			</CardContent>
+		</Card>
+	);
+}
+
+function DateTimeInputs() {
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle>Date/Time Inputs</CardTitle>
+				<CardDescription>Text input fields for selecting dates and times</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<Stack>
+					<LabelledControl label="Date Input">
+						<InputDate placeholder="Pick a date" value="2024-12-25" />
+					</LabelledControl>
+					<LabelledControl label="Time Input">
+						<InputTime placeholder="Pick a time" value="13:15" />
+					</LabelledControl>
+					<LabelledControl label="Date Time Input">
+						<InputDateTime placeholder="Pick a date" value="2024-12-25T13:15" />
+					</LabelledControl>
+					<LabelledControl label="Week Input">
+						<InputWeek placeholder="Pick a week" value="2024-W52" />
+					</LabelledControl>
+					<LabelledControl label="Month Input">
+						<InputMonth placeholder="Pick a month" value="2024-12" />
+					</LabelledControl>
+				</Stack>
+			</CardContent>
+		</Card>
+	);
+}
+
+function TextInputVariations() {
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle>Text Inputs</CardTitle>
+				<CardDescription>
+					Text input fields with built-in validation or semnatic differences
+				</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<Stack>
+					<LabelledControl label="Email Input">
+						<Input type="email" />
+					</LabelledControl>
+					<LabelledControl label="Number Input">
+						<Input type="number" />
+					</LabelledControl>
+					<LabelledControl label="Password Input">
+						<Input type="password" />
+					</LabelledControl>
+					<LabelledControl label="Search Input">
+						<Input type="search" />
+					</LabelledControl>
+					<LabelledControl label="Telephone Input">
+						<Input type="tel" />
+					</LabelledControl>
+					<LabelledControl label="URL Input">
+						<Input type="url" />
+					</LabelledControl>
+				</Stack>
+			</CardContent>
+		</Card>
+	);
+}
+
+function MiscInputVariations() {
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle>Miscellaneous Inputs</CardTitle>
+				<CardDescription>Non-Textual Inputs</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<Stack>
+					<LabelStack>
+						<Label>Color Input</Label>
+						<Input type="color" />
+					</LabelStack>
+					<LabelStack>
+						<Label>File Input</Label>
+						<Input type="file" />
+					</LabelStack>
+
+					<LabelStack>
+						<Label>Range Input</Label>
+						<Input type="range" />
+					</LabelStack>
+				</Stack>
+			</CardContent>
+		</Card>
+	);
+}
+
+export function InputsDemo() {
+	return (
+		<>
+			<InputStates />
+			<DateTimeInputs />
+			<TextInputVariations />
+			<MiscInputVariations />
+		</>
+	);
+}
--- src/shared/components/input.tsx
+++ src/shared/components/input.tsx
@@ -6,21 +6,72 @@ import {
 	mergeFormElementProps,
 } from '~/shared/components/form-element-props';
 
-export interface InputProps extends FormElementProps<'input'> {
+export interface InputProps extends Omit<FormElementProps<'input'>, 'type'> {
+	/** Input type -- intentionally excluding some inputs for which there's a better component */
+	type?:
+		| 'color'
+		| 'date'
+		| 'datetime-local'
+		| 'email'
+		| 'file'
+		| 'month'
+		| 'number'
+		| 'password'
+		| 'range'
+		| 'search'
+		| 'tel'
+		| 'text'
+		| 'time'
+		| 'url'
+		| 'week'
+		| undefined;
 	/** Removes default styling */
 	unstyled?: boolean | undefined;
 }
 
+/** General text input */
 export function Input(props: InputProps) {
 	const [local, rest] = splitProps(props, ['unstyled']);
 	const formControlProps = mergeFormElementProps<'input'>(rest);
 	return <input {...formControlProps} class={cx(!local.unstyled && 'c-input', props.class)} />;
 }
 
+/**
+ * Just a wrapper around existing input but it's a separate component in case we want to
+ * do something behond the default date picker
+ */
 export function InputDate(props: InputProps) {
 	return <Input {...props} type="date" />;
 }
 
+/**
+ * Just a wrapper around existing input but it's a separate component in case we want to
+ * do something behond the default time picker
+ */
 export function InputTime(props: InputProps) {
 	return <Input {...props} type="time" />;
+}
+
+/**
+ * Just a wrapper around existing input but it's a separate component in case we want to
+ * do something behond the default datetime picker
+ */
+export function InputDateTime(props: InputProps) {
+	return <Input {...props} type="datetime-local" />;
+}
+
+/**
+ * Just a wrapper around existing input but it's a separate component in case we want to
+ * do something behond the default month picker
+ */
+export function InputMonth(props: InputProps) {
+	return <Input {...props} type="month" />;
+}
+
+/**
+ * Just a wrapper around existing input but it's a separate component in case we want to
+ * do something behond the default week picker
+ */
+export function InputWeek(props: InputProps) {
+	return <Input {...props} type="week" />;
 }
