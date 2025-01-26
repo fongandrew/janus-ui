52bbf9274d7196b68ec6a38617603e815d58de73
Andrew Fong <afong@slack-corp.com>
2025-01-25 21:49:35 -0800

    Scroll to error on appearance

--- src/shared/components/form-error.tsx
+++ src/shared/components/form-error.tsx
@@ -1,4 +1,4 @@
-import { Show, useContext } from 'solid-js';
+import { createEffect, createSignal, Show, useContext } from 'solid-js';
 
 import { DangerAlert } from '~/shared/components/alert';
 import { FormContext } from '~/shared/components/form-context';
@@ -7,9 +7,17 @@ export function FormError() {
 	const context = useContext(FormContext);
 	const error = context?.errorSig[0];
 
+	const [alertRef, setAlertRef] = createSignal<HTMLDivElement | null>(null);
+	createEffect(() => {
+		const alert = alertRef();
+		if (error?.() && alert) {
+			alert.scrollIntoView({ block: 'nearest' });
+		}
+	});
+
 	return (
 		<Show when={error?.()}>
-			<DangerAlert>{error?.()}</DangerAlert>
+			<DangerAlert ref={setAlertRef}>{error?.()}</DangerAlert>
 		</Show>
 	);
 }
