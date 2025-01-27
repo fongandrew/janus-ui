e73311d1b5eca371c3d7839edeff5003ae9fe271
Andrew Fong <afong@slack-corp.com>
2025-01-27 11:45:16 -0800

    Comments

--- src/shared/components/form-error.tsx
+++ src/shared/components/form-error.tsx
@@ -3,6 +3,10 @@ import { createEffect, createSignal, Show, useContext } from 'solid-js';
 import { DangerAlert } from '~/shared/components/alert';
 import { FormContext } from '~/shared/components/form-context';
 
+/**
+ * A component that displays an error message if the form has an error.
+ * The error message is passed through context from the FormContext provider.
+ */
 export function FormError() {
 	const context = useContext(FormContext);
 	const error = context?.errorSig[0];
--- src/shared/components/label-stack.tsx
+++ src/shared/components/label-stack.tsx
@@ -3,6 +3,7 @@ import { type JSX } from 'solid-js';
 
 import { FormElementProvider } from '~/shared/components/form-element-provider';
 
+/** A stack of labels for form elements. */
 export function LabelStack(props: JSX.HTMLAttributes<HTMLDivElement>) {
 	return (
 		<FormElementProvider>
