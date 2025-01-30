846a7f0044d7871139b9760e968c9dc06aadeaa2
Andrew Fong <afong@slack-corp.com>
2025-01-30 12:19:24 -0800

    Use PostCSS simple vars for breakpoints
    Since CSS variables don't work for media queries

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 import autoprefixer from 'autoprefixer';
 import mixins from 'postcss-mixins';
+import vars from 'postcss-simple-vars';
 import tailwindCSS from 'tailwindcss';
 
 export default {
-	plugins: [mixins, tailwindCSS, autoprefixer],
+	plugins: [mixins, vars, tailwindCSS, autoprefixer],
 };
