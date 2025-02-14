6b6f563a2df060f5418a3a5c7a63dbe68864daa5
Andrew Fong <afong@slack-corp.com>
2025-02-13 17:02:26 -0800

    No longer need simple vars, just using mixins

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 import autoprefixer from 'autoprefixer';
 import mixins from 'postcss-mixins';
-import vars from 'postcss-simple-vars';
 
 export default {
-	plugins: [mixins, vars, autoprefixer],
+	plugins: [mixins, autoprefixer],
 };
