085ca9e66836b7180d50ee6989868135cea9015a
Andrew Fong <afong@slack-corp.com>
2025-02-18 11:59:13 -0800

    Contrast tweaks
    Everything passes-ish
    Some tweaks to details since text is long but bold doesn't look great and gray bg is too low contrast.

--- src/colors.tsx
+++ src/colors.tsx
@@ -10,13 +10,82 @@ import { App } from '~/app';
 import { Card, CardContent, CardHeader, CardTitle } from '~/shared/components/card';
 import { createIncrSignal } from '~/shared/utility/solid/create-incr-signal';
 
+/** Returns minimum APCA threshold for font size / weight combo for "fluent" text */
+function minContrast(fontSizePx: number, fontWeight: number) {
+	if (fontSizePx < 14) return 100;
+	if (fontSizePx <= 14) {
+		if (fontWeight <= 400) return 100;
+		if (fontWeight <= 500) return 100;
+		if (fontWeight <= 600) return 90;
+		return 75;
+	}
+	if (fontSizePx <= 15) {
+		if (fontWeight <= 400) return 100;
+		if (fontWeight <= 500) return 90;
+		if (fontWeight <= 600) return 75;
+		return 70;
+	}
+	if (fontSizePx <= 16) {
+		if (fontWeight <= 400) return 90;
+		if (fontWeight <= 500) return 75;
+		if (fontWeight <= 600) return 70;
+		return 60;
+	}
+	if (fontSizePx <= 18) {
+		if (fontWeight <= 300) return 100;
+		if (fontWeight <= 400) return 75;
+		if (fontWeight <= 500) return 70;
+		if (fontWeight <= 600) return 60;
+		return 55;
+	}
+	if (fontSizePx <= 21) {
+		if (fontWeight <= 300) return 90;
+		if (fontWeight <= 400) return 70;
+		if (fontWeight <= 500) return 60;
+		if (fontWeight <= 600) return 55;
+		return 50;
+	}
+	// Font-size 24 in APCA table
+	if (fontWeight <= 300) return 75;
+	if (fontWeight <= 400) return 60;
+	if (fontWeight <= 500) return 55;
+	if (fontWeight <= 600) return 50;
+	return 45;
+}
+
+/**
+ * With modifications based on usage type:
+ * - Body text: Paragraphs, many blocks of text
+ * - Descriptive text: Also "fluent text", short blocks of text. This 2.5 lines in the APCA
+ *   guidelines but can be a little loose with this with tables and wrapping.
+ * - Label: Single lines, often in menus or other constrained spaces
+ * - Subheading: Larger text, sometimes used in menus or other constrained spaces
+ * - Heading: Largest text
+ */
+function minContrastForUsage(
+	fontSizePx: number,
+	fontWeight: number,
+	usage: 'body' | 'descriptive' | 'label' | 'subheading' | 'heading' = 'descriptive',
+) {
+	const base = minContrast(fontSizePx, fontWeight);
+
+	// APCA rules are actually to add 15 if min is less than 75 but this is sort of weird since
+	// it means increasing font size (which lowers the required contrast on the table) can
+	// increase contrast requirements.
+	if (usage === 'body') return Math.min(base, 75);
+	if (usage === 'descriptive') return base;
+	return Math.max(base - 15, 40);
+}
+
 function ColorWithAPCA(props: {
 	as?: string;
 	class?: string;
 	href?: string;
+	usage?: 'body' | 'descriptive' | 'label' | 'subheading' | 'heading' | undefined;
 	children: JSX.Element;
 }) {
 	const [apca, setAPCA] = createSignal<number | undefined>();
+	const [minAPCA, setMinAPCA] = createSignal<number>(Infinity);
 
 	let ref: HTMLDivElement | undefined;
 
@@ -24,8 +93,11 @@ function ColorWithAPCA(props: {
 		if (!ref) return;
 		const section = ref.closest('section');
 		if (!section) return;
-		const bg = getComputedStyle(section).backgroundColor;
-		const fg = getComputedStyle(ref).color;
+
+		const sectionStyle = getComputedStyle(section);
+		const bg = sectionStyle.backgroundColor;
+		const refStyle = getComputedStyle(ref);
+		const fg = refStyle.color;
 		const [bgR, bgG, bgB] = colorParsley(bg);
 		const [fgR, fgG, fgB, fgA] = colorParsley(fg);
 		const value = APCAcontrast(
@@ -33,6 +105,10 @@ function ColorWithAPCA(props: {
 			sRGBtoY([bgR, bgG, bgB]),
 		);
 		setAPCA(typeof value === 'number' ? value : parseFloat(value));
+
+		const fontSizePx = parseInt(refStyle.fontSize);
+		const fontWeight = parseInt(refStyle.fontWeight);
+		setMinAPCA(minContrastForUsage(fontSizePx, fontWeight, props.usage));
 	});
 
 	const apcaNorm = () => Math.abs(apca() ?? 0);
@@ -55,6 +131,7 @@ function ColorWithAPCA(props: {
 				{apcaNorm() >= 40 && '●'}
 			</span>
 			<span style={{ width: '7ch' }}>({apca()?.toFixed(2)})</span>
+			<span title={`Minimum: ${minAPCA()}`}>{apcaNorm() >= minAPCA() ? '✓' : '✗'}</span>
 		</div>
 	);
 }
@@ -65,17 +142,26 @@ function ColorBox(props: {
 	noDanger?: boolean;
 	noLink?: boolean;
 	noMuted?: boolean;
+	usage?: 'body' | 'descriptive' | 'label' | 'subheading' | 'heading';
 }) {
 	return (
 		<section class={cx('o-text-box', 't-outer-radius', props.class)}>
-			<ColorWithAPCA>{props.label}</ColorWithAPCA>
-			{!props.noDanger && <ColorWithAPCA class="t-text-danger">Danger text</ColorWithAPCA>}
+			<ColorWithAPCA usage={props.usage}>{props.label}</ColorWithAPCA>
+			{!props.noDanger && (
+				<ColorWithAPCA class="t-text-danger" usage="label">
+					Danger text
+				</ColorWithAPCA>
+			)}
 			{!props.noLink && (
-				<ColorWithAPCA as="a" href="#">
+				<ColorWithAPCA as="a" href="#" usage="label">
 					Link text
 				</ColorWithAPCA>
 			)}
-			{!props.noMuted && <ColorWithAPCA class="t-text-muted">Muted text</ColorWithAPCA>}
+			{!props.noMuted && (
+				<ColorWithAPCA class="t-text-muted" usage="label">
+					Muted text
+				</ColorWithAPCA>
+			)}
 		</section>
 	);
 }
@@ -226,10 +312,17 @@ function Main() {
 				<Show when={value()} keyed>
 					{(_count: number) => (
 						<Card class="o-box o-grid">
-							<ColorBox class="v-card-colors" label="Card" />
-							<ColorBox class="v-default-colors" label="Default" />
-							<ColorBox class="v-code-colors" label="Code" noDanger noLink noMuted />
-							<ColorBox class="v-pre-colors" label="Pre" />
+							<ColorBox class="v-card-colors" label="Card" usage="body" />
+							<ColorBox class="v-default-colors" label="Default" usage="body" />
+							<ColorBox
+								class="v-code-colors"
+								label="Code"
+								noDanger
+								noLink
+								noMuted
+								usage="label"
+							/>
+							<ColorBox class="v-pre-colors" label="Pre" usage="label" />
 							<ColorBox class="v-popover-colors" label="Popover" />
 							<ColorBox class="v-tooltip-colors" label="Tooltip" noLink />
 							<ColorBox
@@ -239,13 +332,33 @@ function Main() {
 								noLink
 								noMuted
 							/>
-							<ColorBox class="v-secondary-colors" label="Secondary" />
+							<ColorBox class="v-secondary-colors" label="Secondary" usage="label" />
 							<ColorBox class="v-callout-colors" label="Callout" />
-							<ColorBox class="v-highlight-colors" label="Highlight" noLink />
+							<ColorBox
+								class="v-highlight-colors"
+								label="Highlight"
+								noLink
+								usage="label"
+							/>
 							<ColorBox class="v-input-colors" label="Input" />
-							<ColorBox class="v-success-colors" label="Success" noDanger />
-							<ColorBox class="v-warning-colors" label="Warning" noDanger />
-							<ColorBox class="v-danger-colors" label="Danger" noDanger />
+							<ColorBox
+								class="v-success-colors"
+								label="Success"
+								noDanger
+								usage="label"
+							/>
+							<ColorBox
+								class="v-warning-colors"
+								label="Warning"
+								noDanger
+								usage="label"
+							/>
+							<ColorBox
+								class="v-danger-colors"
+								label="Danger"
+								noDanger
+								usage="label"
+							/>
 						</Card>
 					)}
 				</Show>
