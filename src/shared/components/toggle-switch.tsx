961b1fcf85352ef6f3864a9caf2ae8691fe7e4fd
Andrew Fong <afong@slack-corp.com>
2025-03-08 21:41:26 -0800

    Refactor toggle switch

--- src/shared/components/toggle-switch.tsx
+++ src/shared/components/toggle-switch.tsx
@@ -6,20 +6,14 @@ import {
 	type FormElementProps,
 	mergeFormElementProps,
 } from '~/shared/components/form-element-props';
+import { toggleSwitchChange, toggleSwitchEnter } from '~/shared/handlers/toggle-switch';
+import { callbackAttrs } from '~/shared/utility/callback-registry';
 
 export interface ToggleSwitchProps extends Omit<FormElementProps<'input'>, 'type'> {}
 
 export function ToggleSwitch(props: ToggleSwitchProps) {
 	const [local, rest] = splitProps(props, ['class']);
 	const formProps = mergeFormElementProps<'input'>(rest);
-
-	// Enter conflicts with form submit but toggle generally isn't used in forms
-	// and treated more like a button
-	const handleKeyDown = (event: KeyboardEvent) => {
-		if (event.key === 'Enter') {
-			(event.target as HTMLInputElement)?.toggleAttribute('checked');
-		}
-	};
 
 	return (
 		<div class={cx('c-toggle-switch', local.class)} onClick={handleClick}>
@@ -28,8 +22,8 @@ export function ToggleSwitch(props: ToggleSwitchProps) {
 				role="switch"
 				aria-checked={props.checked}
 				aria-readonly={props.readOnly}
-				onKeyDown={handleKeyDown}
 				{...formProps}
+				{...callbackAttrs(props, toggleSwitchChange, toggleSwitchEnter)}
 			/>
 			<div class="c-toggle-switch__thumb" />
 		</div>
---
+++ src/shared/handlers/toggle-switch.ts
@@ -0,0 +1,25 @@
+import { createHandler } from '~/shared/utility/event-handler-attrs';
+
+/**
+ * Treat enter key as a click on the toggle switch. Because toggle is input, general browser
+ * behavior is to ignore enter because enter is for submitting forms. But toggle switch
+ * functions more like button than input (and is rarely in a form anyways).
+ */
+export const toggleSwitchEnter = createHandler('keydown', 'toggle-switch__enter', (event) => {
+	if (event.key === 'Enter') {
+		event.currentTarget.click();
+		event.preventDefault();
+	}
+});
+
+/**
+ * Synchronize the toggle switch's `aria-checked` attribute with the `checked` property.
+ * Kinda unclear whether this is necessary if underlying element *is* a checkbox
+ * but https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Reference/Roles/switch_role
+ * says `aria-checked` is required.
+ */
+export const toggleSwitchChange = createHandler('change', 'toggle-switch__change', (event) => {
+	const target = event.currentTarget as HTMLElement;
+	const checked = (target as HTMLInputElement).checked;
+	target.setAttribute('aria-checked', checked.toString());
+});
