830460f3f433bcd99a78f9076a3ddd874646edb6
Andrew Fong <afong@slack-corp.com>
2025-03-14 11:50:12 -0700

    Type shouldn't include prefix

--- src/shared/components/form-element-props.ts
+++ src/shared/components/form-element-props.ts
@@ -51,7 +51,7 @@ export type FormElementProps<
 	/**
 	 * Allow arbitrary data attributes
 	 */
-	[key: `data-c-${string}`]: string | undefined;
+	[key: `data-${string}`]: string | undefined;
 };
 
 /** Rewrites contextual props input elements and other form-like controls */
