e68ef96cd533e544d4c22f0d5a50f854f0b2824f
Andrew Fong <afong@slack-corp.com>
2025-03-13 21:41:40 -0700

    Fix tests so they work with SSR

--- vite.config.ts
+++ vite.config.ts
@@ -5,47 +5,51 @@ import { defineConfig } from 'vitest/config';
 import purgeCSSPlugin from './plugins/vite-plugin-purgecss';
 import viteSSGPlugin from './plugins/vite-plugin-ssg';
 
-export default defineConfig({
-	plugins: [
-		solidPlugin({ ssr: true }),
-		purgeCSSPlugin({
-			variables: true,
-			keyframes: true,
-			fontFace: true,
+export default defineConfig(({ mode }) => {
+	const isTest = mode === 'test';
 
-			// Needed to keep nested selectors from getting purged
-			// https://github.com/FullHuman/purgecss/issues/1153#issuecomment-2626375284
-			safelist: ['&'],
-		}),
-		viteSSGPlugin(),
-	],
-	resolve: {
-		alias: {
-			'~': '/src',
+	return {
+		plugins: [
+			solidPlugin({ ssr: !isTest }),
+			purgeCSSPlugin({
+				variables: true,
+				keyframes: true,
+				fontFace: true,
+
+				// Needed to keep nested selectors from getting purged
+				// https://github.com/FullHuman/purgecss/issues/1153#issuecomment-2626375284
+				safelist: ['&'],
+			}),
+			viteSSGPlugin(),
+		],
+		resolve: {
+			alias: {
+				'~': '/src',
+			},
 		},
-	},
-	build: {
-		outDir: 'dist',
-		rollupOptions: {
-			input: Object.fromEntries(
-				readdirSync('.')
-					.filter(
-						(file) =>
-							// Must be HTML file
-							file.endsWith('.html') &&
-							// Must be a file (not directory)
-							statSync(file).isFile(),
-					)
-					.map((file) => [file.slice(0, -5), file]),
-			),
+		build: {
+			outDir: 'dist',
+			rollupOptions: {
+				input: Object.fromEntries(
+					readdirSync('.')
+						.filter(
+							(file) =>
+								// Must be HTML file
+								file.endsWith('.html') &&
+								// Must be a file (not directory)
+								statSync(file).isFile(),
+						)
+						.map((file) => [file.slice(0, -5), file]),
+				),
+			},
 		},
 		target: 'esnext',
-	},
-	server: { port: 3000, hmr: { port: 3000 } },
-	test: {
-		globals: true,
-		environment: 'jsdom',
-		clearMocks: true,
-		setupFiles: 'vitest.setup.ts',
-	},
+		server: { port: 3000, hmr: { port: 3000 } },
+		test: {
+			globals: true,
+			environment: 'jsdom',
+			clearMocks: true,
+			setupFiles: 'vitest.setup.ts',
+		},
+	};
 });
