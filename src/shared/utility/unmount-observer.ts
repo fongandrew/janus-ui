dffadb51d31c0091eb18e32eb9f6a85ec023b9c9
Andrew Fong <afong@slack-corp.com>
2025-03-17 15:35:26 -0700

    Fix unmount observer to handle deep ancestors

--- src/shared/utility/unmount-observer.ts
+++ src/shared/utility/unmount-observer.ts
@@ -1,24 +1,78 @@
-export function onUnmount(
-	element: HTMLElement,
-	callback: (element: HTMLElement) => void,
-): () => void {
-	const observer = new MutationObserver((mutations) => {
-		for (const mutation of mutations) {
-			if (
-				mutation.type === 'childList' &&
-				Array.from(mutation.removedNodes).includes(element)
-			) {
-				callback(element);
-				observer.disconnect();
-				break;
-			}
-		}
-	});
+import { createMagicProp } from '~/shared/utility/magic-prop';
+import { elmDoc } from '~/shared/utility/multi-view';
 
-	const parentElement = element.parentElement;
-	if (parentElement) {
-		observer.observe(parentElement, { childList: true });
+/** Magic prop with list of elements callback per document */
+const [elmList, setElmList] = createMagicProp<HTMLElement[]>();
+
+/** Magic prop for tracking list of callbacks per element */
+const [callbacks, setCallbacks] = createMagicProp<((element: HTMLElement) => void)[]>();
+
+/** Magic prop tracking observers per document */
+const [docObserver, setDocObserver] = createMagicProp<MutationObserver>();
+
+export function onUnmount(element: HTMLElement, callback: (element: HTMLElement) => void) {
+	const document = elmDoc(element);
+	if (!document) return;
+
+	let lst = elmList(document);
+	if (!lst) {
+		lst = [];
+		if (lst.indexOf(element) === -1) {
+			lst.push(element);
+		}
+		setElmList(document, lst);
 	}
 
-	return () => observer.disconnect();
+	let callbackList = callbacks(element);
+	if (!callbackList) {
+		callbackList = [];
+		setCallbacks(element, callbackList);
+	}
+	callbackList.push(callback);
+
+	let observer = docObserver(document);
+	if (observer) return;
+
+	// This basically runs on every mutation. We can't just monitor the parent node or some
+	// smaller subtree because any ancestor could be removed. This should be fine though
+	// so long as the number of elements being tracked is small.
+	observer = new MutationObserver(() => {
+		const lst = elmList(document);
+		if (!lst) return;
+
+		const toRemoved: HTMLElement[] = [];
+
+		// Squeeze a little extra speed out of this since we're maybe calling
+		// this particular loop a lot (on every mutation)
+		// eslint-disable-next-line @typescript-eslint/prefer-for-of
+		for (let i = 0; i < lst.length; i++) {
+			const elm = lst[i];
+			if (!elm || elm.isConnected) continue;
+
+			toRemoved.push(elm);
+
+			const callbackList = callbacks(elm);
+			if (!callbackList) continue;
+
+			for (const callback of callbackList) {
+				callback(elm);
+			}
+		}
+
+		if (toRemoved.length) {
+			for (const elm of toRemoved) {
+				const idx = lst.indexOf(elm);
+				if (idx !== -1) {
+					lst.splice(idx, 1);
+				}
+			}
+		}
+
+		if (lst.length) return;
+		observer?.disconnect();
+		setDocObserver(document, undefined);
+	});
+
+	setDocObserver(document, observer);
+	observer.observe(document.body, { childList: true, subtree: true });
 }
