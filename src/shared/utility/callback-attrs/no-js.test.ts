d53ca30b40eff7a48a414dfd196379eb361528c2
Andrew Fong <afong@slack-corp.com>
2025-03-31 13:57:18 -0700

    no-js helper test

---
+++ src/shared/utility/callback-attrs/no-js.test.ts
@@ -0,0 +1,38 @@
+import { setAttrs } from '~/shared/utility/attribute';
+import { callbackAttrs } from '~/shared/utility/callback-attrs/callback-registry';
+import { processRoot } from '~/shared/utility/callback-attrs/mount';
+import { mountAttr, mountRmAttr } from '~/shared/utility/callback-attrs/no-js';
+import { mount } from '~/shared/utility/test-utils/mount';
+
+describe('no-js callback attributes', () => {
+	describe('mountAttr', () => {
+		it('should set attribute on element when processRoot is called', () => {
+			const elm = document.createElement('div');
+
+			setAttrs(
+				elm,
+				callbackAttrs(mountAttr('data-test', 'value'), mountAttr('aria-hidden', 'true')),
+			);
+			mount(elm);
+
+			processRoot(document);
+
+			expect(elm.getAttribute('data-test')).toBe('value');
+			expect(elm.getAttribute('aria-hidden')).toBe('true');
+		});
+	});
+
+	describe('mountRmAttr', () => {
+		it('should remove attribute from element when processRoot is called', () => {
+			const elm = document.createElement('div');
+			elm.setAttribute('data-test', 'remove-me');
+
+			setAttrs(elm, callbackAttrs(mountRmAttr('data-test')));
+			mount(elm);
+
+			processRoot(document);
+
+			expect(elm.hasAttribute('data-test')).toBe(false);
+		});
+	});
+});
