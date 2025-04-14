a6ab26e5ab59b3da4f62dbc869ed836a08b7aa4d
Andrew Fong <afong@slack-corp.com>
2025-04-13 19:43:38 -0700

    Label clicks acutally click
    Motivation is to trigger toggles with label clicks
    but we're also throwing in selection as well

--- src/shared/components/callbacks/label.ts
+++ src/shared/components/callbacks/label.ts
@@ -1,4 +1,5 @@
 import { createHandler } from '~/shared/utility/callback-attrs/events';
+import { focusAndSelect } from '~/shared/utility/focus-and-select';
 import { elmDoc } from '~/shared/utility/multi-view';
 
 /**
@@ -12,9 +13,15 @@ export const focusInputOnClick = createHandler('click', '$c-label__focus-input',
 	const document = elmDoc(labelLike);
 
 	const input = document?.querySelector<HTMLInputElement>(`[aria-labelledby~="${labelLike.id}"]`);
-	input?.focus();
+	if (!input) return;
 
-	// Also show the popover if there is one
+	input.click();
+
+	// Clicking should auto-focus a text input but it doesn't always?
+	focusAndSelect(input);
+
+	// Likewise, it should also show popover, but this doesn't reliably happen in tests,
+	// so manually show the popover if there is one
 	const popoverTarget = input?.getAttribute('popovertarget');
 	if (!popoverTarget) return;
 
